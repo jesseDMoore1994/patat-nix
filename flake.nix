b52c15f10a71cf5d5749acc3cc97192da49b6138
Jesse Moore <jesse@jessemoore.dev>
2023-01-29 16:36:05 -0600

    complete build

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   description = "A flake for building patat";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
     patat-src.url = "github:jaspervdj/patat";
+    patat-src.flake = false;
   };
 
   outputs = { self, nixpkgs, flake-utils, patat-src }:
@@ -12,25 +13,23 @@
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [myNixPkgsOverlay];
         }; 
+
+        myNixPkgsOverlay = (nixSelf: nixSuper: {
+          myHaskellPackages = nixSelf.haskellPackages.override (oldHaskellPkgs: {
+            overrides = nixSelf.lib.composeExtensions (oldHaskellPkgs.overrides or (_: _: {}))  myHaskellPkgsOverlay;
+          });
+        });
+
+        myHaskellPkgsOverlay = (hSelf: hSuper: {
+          # "myproject" is the first part of the "myproject.cabal" project definition file
+          patat = hSelf.callCabal2nix "patat" patat-src {};
+        });
+
       in rec {
-        packages = flake-utils.lib.flattenTree {
-          patat = pkgs.stdenv.mkDerivation {
-            pname = "patat";
-            version = patat-src.rev;
-            src = patat-src;
-            buildInputs = [
-              pkgs.cabal-install
-              pkgs.ghc
-              pkgs.ghcid
-            ];
-            buildPhase = ''
-              cabal build
-            '';
-          };
-        };
-        defaultPackage = packages.patat;
-        apps.patat = flake-utils.lib.mkApp { drv = packages.patat; };
+        defaultPackage = pkgs.myHaskellPackages.patat;
+        apps.patat = flake-utils.lib.mkApp { drv = pkgs.myHaskellPkgsOverlay.patat; };
         defaultApp = apps.patat;
       }
     );
